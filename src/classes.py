22c2dd68806422d4ac0b281f9622584eeb72b712
Сергей Бутаков <xolodnotchk@mail.ru>
2024-03-06 10:41:25 +0300

    удаление не нужных строк

--- src/classes.py
+++ src/classes.py
@@ -4,7 +4,7 @@ class Category:
     description: str
     products: list
     total_categories = 0
-    total_unique_products = 0
+    total_unique_products = set()
 
     def __init__(self, name, description, products):
         self.name = name
@@ -29,9 +29,3 @@ class Product:
         self.quantity = quantity
 
         Product.total_products += 1
-
-
-# cat_1 = Category('Фрукты', 'Семечковые', ['Абрикос' 'Вишня' 'Яблоко' 'Слива'])
-# cat_2 = Category('Фрукты', 'Ягоды', ['Виноград', 'Клубника', 'Крыжовник'])
-# print(Category.total_categories)
-# print(cat_1.name)
